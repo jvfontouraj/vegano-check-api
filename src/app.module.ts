6c47d55b78e320d0b1a9adb6b35edc8e0bc83927
João Vitor F Jardim <joao@nomatch.studio>
2023-11-08 16:56:33 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://jvfontouraj:pFPSixwlciQffmWq@cluster0.ufzuksl.mongodb.net/?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.ufzuksl.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
     ),
     UsersModule,
   ],
